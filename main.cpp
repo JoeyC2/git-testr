e08dda9d55360e0cd4a3bedbde2099470af75f90
JOEYC2 <jchesso2@ut.utm.edu>
2026-02-10 15:58:48 -0600

    nemain

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+ #include <iostream>
+ #include <iomanip>
+
+ using namespace std;
+
+ void sum(int var2, int var1){
+  double add = var2 + var1;
+  double ave = add / 2;
+  cout<<ave;
+ }
+
+ int main() {
+  int var2 = 0;
+  int var1 = 0;
+  cout<<fixed<<setprecision(2);
+  cout<<"enter the numbers to be averaged"<<endl;
+  cin>>var1;
+  cout<<endl<<"enter the second number"<<endl;
+  cin>>var2;
+  cout<<endl;
+  sum(var2, var1);
+}
 
